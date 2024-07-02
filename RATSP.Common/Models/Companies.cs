11b7f75e5ee2d73b2f1575ffdaca726d4b639d0b
VVSaltykov <vitya.saltyckov@gmail.com>
2024-07-02 11:58:20 +0300

    ~

---
+++ RATSP.Common/Models/Companies.cs
@@ -0,0 +1,18 @@
+﻿namespace RATSP.Common.Models;
+
+public class Companies
+{
+    public string Name { get; set; }
+    public int Number { get; set; }
+    public bool Participation { get; set; }
+    public string Position { get; set; }
+    public string FIO { get; set; }
+    public string INN { get; set; }
+    public string KPP { get; set; }
+    public string PC { get; set; }
+    public string BankName { get; set; }
+    public string BIK { get; set; }
+    public string KC { get; set; }
+    public string Address { get; set; }
+    public string MailAddress { get; set; }
+}
--- RATSP2/RATSP2/AppDbContext.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace RATSP2;
-
-public class AppDbContext : DbContext
-{
-    
-}
