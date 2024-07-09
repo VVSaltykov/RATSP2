863e4885f99395b2670639882e19942d967346aa
VVSaltykov <vitya.saltyckov@gmail.com>
2024-07-09 12:20:30 +0300

    added excel data collection

---
+++ RATSP.WebCommon/Models/ExcelValues.cs
@@ -0,0 +1,32 @@
+﻿namespace RATSP.WebCommon.Models;
+
+public class ExcelValues
+{
+    public string? Number { get; set; }
+    public string? Insurer { get; set; }
+    public string? Policyholder { get; set; }
+    public string? ContractNumber { get; set; }
+    public string? StartDate { get; set; }
+    public string? EndDate { get; set; }
+    public string? Currency { get; set; }
+    public string? InsuranceAmount_LiabilityLimit { get; set; }
+    public string? AccruedBonus100 { get; set; }
+    public string? GrossPremium { get; set; }
+    public string? ReinsurerCommission { get; set; }
+    public string? ReinsurerCommissionPercent { get; set; }
+    public string? AdministratorCommissionPercent { get; set; }
+    public string? AdministratorCommission { get; set; }
+    public string? NetPremium { get; set; }
+    public string? PremiumPercent { get; set; }
+    public string? PaymentRate_ReturnRate { get; set; }
+    public string? RefundPremium { get; set; }
+    public string? AdministratorCommissionRub { get; set; }
+    public string? SanctionsRisk { get; set; }
+    public string? ReinsurerFraction { get; set; }
+    public string? PaymentDate { get; set; }
+    public string? PaymentSumm { get; set; }
+    public string? PaymentNumber { get; set; }
+    public string? Comment { get; set; }
+    public string? InsuranceType { get; set; }
+    public string? PaymentContract { get; set; }
+}
