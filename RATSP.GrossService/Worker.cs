0498cbc8b6f97c437d21ac786f2aa3a429cd5f93
Салтыков Виктор Владимирович <SaltykovVV@almaz.int>
2024-10-07 17:01:52 +0400

    start consuming

--- RATSP.GrossService/Worker.cs
+++ RATSP.GrossService/Worker.cs
@@ -5,14 +5,17 @@ namespace RATSP.GrossService;
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly KafkaConsumer kafkaConsumer;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(ILogger<Worker> logger, KafkaConsumer kafkaConsumer)
     {
         _logger = logger;
+        this.kafkaConsumer = kafkaConsumer;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        kafkaConsumer.StartConsuming();
         while (!stoppingToken.IsCancellationRequested)
         {
             if (_logger.IsEnabled(LogLevel.Information))
